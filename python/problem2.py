d51feadfcfd688655b9d7789bbd1961065c3737d
rcoy_v <rcoy.vance@gmail.com>
2014-08-24 14:58:52 -0500

    format

--- python/problem2.py
+++ python/problem2.py
@@ -5,5 +5,5 @@ def fibonacci(limit):
         yield a
         a, b = b, a + b
 
-print(sum(x for x in fibonacci(4000000) if x % 2 == 0))
 
+print(sum(x for x in fibonacci(4000000) if x % 2 == 0))
--- python/problem3.py
+++ python/problem3.py
@@ -4,23 +4,22 @@ import math
 
 def prime_factors(number):
     max_factor = math.sqrt(number)
-    prime_factors = []
+    prime_factors_list = []
 
     if number % 2 == 0:
         number /= 2
-        prime_factors.append(2)
+        prime_factors_list.append(2)
         while number % 2 == 0:
             number /= 2
     factor = 3
     while number > 1 and factor <= max_factor:
         if number % factor == 0:
             number /= factor
-            prime_factors.append(factor)
+            prime_factors_list.append(factor)
             while number % factor == 0:
                 number /= factor
         factor += 2
-    return prime_factors
+    return prime_factors_list
+
 
 print(prime_factors(600851475143))
-
-
